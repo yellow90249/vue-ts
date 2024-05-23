bb711463be9750b425e7a2ba58478d5374d13b88
gary <yellow90249@gmail.com>
2024-05-23 15:40:32 +0800

    feat: init

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,29 +7,28 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
-  apiKey: 'AIzaSyAdaRMrPlWge1BcUekv1nfFB4KIIdUymvA',
-  authDomain: 'yapay-eb8c2.firebaseapp.com',
-  projectId: 'yapay-eb8c2',
-  storageBucket: 'yapay-eb8c2.appspot.com',
-  messagingSenderId: '283539932719',
-  appId: '1:283539932719:web:04a758dcd8ff1efc94a8da',
-});
+const firebaseConfig = {
+  apiKey: 'AIzaSyA4_9q1RCXpjgq3YCL0mdhjCgskqEMsHhM',
+  authDomain: 'first-project-e227a.firebaseapp.com',
+  projectId: 'first-project-e227a',
+  storageBucket: 'first-project-e227a.appspot.com',
+  messagingSenderId: '896215768638',
+  appId: '1:896215768638:web:24ced2867af9eb9fb4bb6f',
+};
+firebase.initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('test');
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/firebase-logo.png',
+    icon: './confused.png',
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,60 @@
 <template>
   <div>
-    <InputText></InputText>
+    <h1>123</h1>
+    <p>{{ token }}</p>
   </div>
 </template>
 
-<script setup lang="ts">
-import InputText from 'primevue/inputtext';
+<script setup>
+import { ref } from 'vue';
+const token = ref('');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA4_9q1RCXpjgq3YCL0mdhjCgskqEMsHhM',
+  authDomain: 'first-project-e227a.firebaseapp.com',
+  projectId: 'first-project-e227a',
+  storageBucket: 'first-project-e227a.appspot.com',
+  messagingSenderId: '896215768638',
+  appId: '1:896215768638:web:24ced2867af9eb9fb4bb6f',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging();
+
+// Need notification permission
+getToken(messaging, {
+  vapidKey:
+    'BAhp-0pFJ_8XhddWDVnSCtV0Y1sH0fGfNl5uD4VLFvRVdw02pIMwcfK_p0yQbUM6nrYAmHFhx39bdoCtJuz4h1s',
+})
+  .then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      console.log(currentToken);
+      token.value = currentToken;
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+    }
+  })
+  .catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    alert('Open browser notification');
+  });
+
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
