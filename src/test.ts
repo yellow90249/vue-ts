700235f65cfe0a4f7bc64ef679f2fe61fd8425e6
gary <yellow90249@gmail.com>
2024-05-23 17:36:19 +0800

    feat: test

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,5 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js');
+import { initializeApp } from 'firebase/app';
+import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
@@ -15,16 +12,16 @@ const firebaseConfig = {
   messagingSenderId: '896215768638',
   appId: '1:896215768638:web:24ced2867af9eb9fb4bb6f',
 };
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();
+const messaging = getMessaging(firebaseApp);
 
-messaging.onBackgroundMessage((payload) => {
+onBackgroundMessage(messaging, (payload: any) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = payload.notification.title + '123';
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
     icon: './confused.png',
