be3179f6ee3a81ac627fb224712edfe610bbc458
gary <yellow90249@gmail.com>
2024-05-23 21:28:01 +0800

    feat: update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,34 +1,35 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-const firebaseConfig = {
-  apiKey: 'AIzaSyA4_9q1RCXpjgq3YCL0mdhjCgskqEMsHhM',
-  authDomain: 'first-project-e227a.firebaseapp.com',
-  projectId: 'first-project-e227a',
-  storageBucket: 'first-project-e227a.appspot.com',
-  messagingSenderId: '896215768638',
-  appId: '1:896215768638:web:24ced2867af9eb9fb4bb6f',
-};
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: 'AIzaSyAdaRMrPlWge1BcUekv1nfFB4KIIdUymvA',
+  authDomain: 'yapay-eb8c2.firebaseapp.com',
+  projectId: 'yapay-eb8c2',
+  storageBucket: 'yapay-eb8c2.appspot.com',
+  messagingSenderId: '283539932719',
+  appId: '1:283539932719:web:04a758dcd8ff1efc94a8da',
+});
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+  console.log('test');
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = payload.notification.title + '123';
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: './confused.png',
+    icon: '/firebase-logo.png',
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
