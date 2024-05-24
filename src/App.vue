87d36577aca3636abc173d5a8894cd459b2b43cb
gary <yellow90249@gmail.com>
2024-05-24 18:10:06 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,8 @@
 </template>
 
 <script setup lang="ts">
-function iOSversion() {
-  if (/iP(hone|od|ad)/.test(navigator.platform)) {
-    // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
-    var v = navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/)!;
-    console.log("🚀 ~ iOSversion ~ v:", v)
-    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-  }
-}
-let res = iOSversion();
-console.log('🚀 ~ res:', res);
+navigator.userAgent;
+console.log('🚀 ~ navigator.userAgent:', navigator.userAgent);
 </script>
 
 <style scoped></style>
