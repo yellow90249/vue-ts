528dac67c830682db5c0ffd152c597307574cc7d
gary <yellow90249@gmail.com>
2024-05-24 18:24:45 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,10 @@
 console.log('🚀 ~ navigator.userAgent:', navigator.userAgent);
 console.log('🚀 ~ navigator.platform:', navigator.platform);
 console.log('🚀 ~ navigator.appVersion:', navigator.appVersion);
-const version = navigator.userAgent.match(/Version\/(\d+\.\d+)/);
-console.log('🚀 ~ version:', version);
+const versionMatch = navigator.userAgent.match(/Version\/(\d+\.\d+)/);
+console.log('🚀 ~ versionMatch:', versionMatch);
+const safariVersion = versionMatch ? versionMatch[1] : null;
+console.log('🚀 ~ safariVersion:', safariVersion);
 </script>
 
 <style scoped></style>
