1e103b898ae52f26b8d8aa16d8b16afed8cb1b7b
gary <yellow90249@gmail.com>
2024-05-30 15:19:04 +0800

    feat: personal fcm

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,50 @@
 <template>
-  <div></div>
+  <div>
+    <h1>123</h1>
+    <p>{{ token }}</p>
+  </div>
 </template>
 
-<script setup lang="ts">
-console.log('🚀 ~ navigator.userAgent:', navigator.userAgent);
-console.log('🚀 ~ navigator.platform:', navigator.platform);
-console.log('🚀 ~ navigator.appVersion:', navigator.appVersion);
-const versionMatch = navigator.userAgent.match(/Version\/(\d+\.\d+)/);
-console.log('🚀 ~ versionMatch:', versionMatch);
-const safariVersion = versionMatch ? versionMatch[1] : null;
-console.log('🚀 ~ safariVersion:', safariVersion);
+<script setup>
+import { ref } from 'vue';
+const token = ref('');
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
-function isIosVersionSupportNotification() {
-  const versionMatch = navigator.userAgent.match(/Version\/(\d+\.\d+)/);
-  const safariVersion = versionMatch ? versionMatch[1] : null;
-  if (Number(safariVersion) >= 16.4) return true;
-  else return false;
-}
+const firebaseConfig = {
+  apiKey: 'AIzaSyA4_9q1RCXpjgq3YCL0mdhjCgskqEMsHhM',
+  authDomain: 'first-project-e227a.firebaseapp.com',
+  projectId: 'first-project-e227a',
+  storageBucket: 'first-project-e227a.appspot.com',
+  messagingSenderId: '896215768638',
+  appId: '1:896215768638:web:24ced2867af9eb9fb4bb6f',
+};
 
-console.log('isIosVersionSupportNotification', isIosVersionSupportNotification());
+const app = initializeApp(firebaseConfig);
+
+const messaging = getMessaging();
+
+getToken(messaging, {
+  vapidKey:
+    'BAhp-0pFJ_8XhddWDVnSCtV0Y1sH0fGfNl5uD4VLFvRVdw02pIMwcfK_p0yQbUM6nrYAmHFhx39bdoCtJuz4h1s',
+})
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log(currentToken);
+      token.value = currentToken;
+    } else {
+      console.log('No registration token available. Request permission to generate one.');
+    }
+  })
+  .catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    alert('Open browser notification');
+  });
+
+onMessage(messaging, (payload) => {
+  console.log('收到啦：', payload);
+  // ...
+});
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
